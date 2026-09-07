d114c2917ced29534b02a6a20643bc1b99046d96
rohmanabdu <amilnawawi9@gmail.com>
2026-09-07 16:36:09 +0700

    Update apkUrl to new GitHub link

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 {
   "versionCode": 2,
   "versionName": "1.0.1",
-  "apkUrl": "https://domain-anda.com/update/app-release.apk",
+  "apkUrl": "https://github.com/rohmanabdu/aplii/raw/refs/heads/main/app-release.apk",
   "sha256": "HASH_SHA256_APK",
   "releaseNotes": [
     "Perbaikan navigasi remote TV",

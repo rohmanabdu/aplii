01310ba1d5fdd8bab59be614229571a7c43c554d
rohmanabdu <amilnawawi9@gmail.com>
2026-09-07 16:30:07 +0700

    Add version.js with version details and release notes

---
+++ version.js
@@ -0,0 +1,12 @@
+{
+  "versionCode": 2,
+  "versionName": "1.0.1",
+  "apkUrl": "https://domain-anda.com/update/app-release.apk",
+  "sha256": "HASH_SHA256_APK",
+  "releaseNotes": [
+    "Perbaikan navigasi remote TV",
+    "Perbaikan player",
+    "Peningkatan performa"
+  ],
+  "mandatory": false
+}
